7d178e6ce3845fc53b7072a927c9db1beec4fbff
Kateria Wynn <iamkateria@gmail.com>
2022-07-04 07:41:57 -0700

    added test file

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-/* 
-Write a function called appendToString, which accepts two strings.
-
-The function should return a new string which consists of the second string appended to the first string.
-
-Examples:
-
-appendToString("Hello", " World!"); // "Hello World!"
-appendToString("Foo", "bar"); // "Foobar"
-appendToString("bar", "Foo"); // "barFoo"
-appendToString("", "test"); // "test"
-appendToString("other test", ""); // "other test"
-*/
+// This is a test file.
