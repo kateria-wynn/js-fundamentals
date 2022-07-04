1139d2a9fe1b7f6a1591198f7baf168232edd4de
Kateria Wynn <iamkateria@gmail.com>
2022-07-04 07:23:14 -0700

    added initial directories

---
+++ 002-prependToString/index.js
@@ -0,0 +1,11 @@
+/* 
+Write a function called prependToString, which accepts two strings.
+
+The function should return a new string with the second string prepended to the first string.
+
+Examples:
+
+prependToString('awesome', 'very') // 'veryawesome'
+prependToString('world', 'hello ') // 'hello world'
+prependToString('nothing', '') // 'nothing'
+*/
