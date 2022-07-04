e6d6edb44056ad475a208a0b1eb83476e190666d
Kateria Wynn <iamkateria@gmail.com>
2022-07-04 07:28:16 -0700

    added instructions via comment

--- 001-appendToString/index.js
+++ 001-appendToString/index.js
@@ -1,7 +1,5 @@
 /* 
-Write a function called appendToString, which accepts two strings.
-
-The function should return a new string which consists of the second string appended to the first string.
+Write a function called appendToString, which accepts two strings. The function should return a new string which consists of the second string appended to the first string.
 
 Examples:
 
--- 002-prependToString/index.js
+++ 002-prependToString/index.js
@@ -1,7 +1,5 @@
 /* 
-Write a function called prependToString, which accepts two strings.
-
-The function should return a new string with the second string prepended to the first string.
+Write a function called prependToString, which accepts two strings. The function should return a new string with the second string prepended to the first string.
 
 Examples:
 
