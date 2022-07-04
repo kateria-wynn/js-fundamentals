3a5ce04e1ff826b13164df3ef63b587499c7e519
Kateria Wynn <iamkateria@gmail.com>
2022-07-03 21:24:35 -0700

    added instructions via comment

---
+++ index.js
@@ -0,0 +1,13 @@
+/* 
+Write a function called appendToString, which accepts two strings.
+
+The function should return a new string which consists of the second string appended to the first string.
+
+Examples:
+
+appendToString("Hello", " World!"); // "Hello World!"
+appendToString("Foo", "bar"); // "Foobar"
+appendToString("bar", "Foo"); // "barFoo"
+appendToString("", "test"); // "test"
+appendToString("other test", ""); // "other test"
+*/
