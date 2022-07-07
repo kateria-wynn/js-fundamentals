9cd2343d601dd6594f8b42e03165a889e496fcef
Kateria Wynn <iamkateria@gmail.com>
2022-07-07 14:26:07 -0700

    add solution

--- 001-appendToString/index.js
+++ 001-appendToString/index.js
@@ -1,5 +1,7 @@
 /* 
-Write a function called appendToString, which accepts two strings. The function should return a new string which consists of the second string appended to the first string.
+Write a function called appendToString, which accepts two strings. 
+The function should return a new string which consists of the second string 
+appended to the first string.
 
 Examples:
 
@@ -9,3 +11,23 @@ appendToString("bar", "Foo"); // "barFoo"
 appendToString("", "test"); // "test"
 appendToString("other test", ""); // "other test"
 */
+
+// str1, space, str2 ✅
+// str1, str2 ✅
+// empty str1, str2 ✅
+// str1, empty str2 ✅
+// There is a string method concat() but will use the addition operator (+)
+// because it has better performance
+
+function appendToString(str1, str2) {
+  // if both are not empty strings
+  if (str1 !== '' && str2 !== '') return str1 + str2;
+  // if str1 or str2 is empty
+  if (str1 === '') return str2;
+  if (str2 === '') return str1;
+}
+appendToString('Hello', ' World!');
+appendToString('Foo', 'bar');
+appendToString('bar', 'Foo');
+appendToString('', 'test');
+appendToString('other test', '');
