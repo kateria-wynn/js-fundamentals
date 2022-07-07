8be3245ae69b77039b29f69f143b5117164520a5
Kateria Wynn <iamkateria@gmail.com>
2022-07-07 15:06:19 -0700

    added solution

--- 002-prependToString/index.js
+++ 002-prependToString/index.js
@@ -1,5 +1,7 @@
 /* 
-Write a function called prependToString, which accepts two strings. The function should return a new string with the second string prepended to the first string.
+Write a function called prependToString, which accepts two strings. 
+The function should return a new string with the second string prepended 
+to the first string.
 
 Examples:
 
@@ -7,3 +9,16 @@ prependToString('awesome', 'very') // 'veryawesome'
 prependToString('world', 'hello ') // 'hello world'
 prependToString('nothing', '') // 'nothing'
 */
+
+// str2, str1 ✅
+// empty str2, str1 ✅
+// There is a string method concat() but will use the addition operator (+)
+// because it has better performance
+
+function prependToString(str1, str2) {
+  if (str2 !== '' && str1 !== '') return str2 + str1;
+  if (str2 === '') return str1;
+}
+prependToString('awesome', 'very');
+prependToString('world', 'hello ');
+prependToString('nothing', '');
