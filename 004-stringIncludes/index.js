a5e35b5dc72109107bb59589779cdfede731a231
Kateria Wynn <iamkateria@gmail.com>
2022-07-07 14:26:39 -0700

    add instructions via comments

---
+++ 004-stringIncludes/index.js
@@ -0,0 +1,12 @@
+/* 
+Write a function called stringIncludes, which accepts two strings: the first string is a word and the second string is a single character.
+
+The function should return true if the first string includes the character, otherwise it should return false.
+
+Do not use the built in String.includes() function!
+
+Examples:
+
+stringIncludes('awesome', 'e'); // true
+stringIncludes('awesome', 'z'); // false
+*/
