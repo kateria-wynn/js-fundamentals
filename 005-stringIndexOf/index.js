7a2e76bfe66c298652fb4bd7d6ceb54186d7a2d1
Kateria Wynn <iamkateria@gmail.com>
2022-07-07 19:47:58 -0700

    added instructions via comment

---
+++ 005-stringIndexOf/index.js
@@ -0,0 +1,12 @@
+/* 
+Write a function called stringIndexOf, which accepts two strings: the first is a word and the second is a single character.
+
+The function should return the first index in the word at which the character exists or -1 if the character is not found.
+
+Do not use the built in String.indexOf() function!
+
+Examples:
+
+stringIndexOf('awesome', 'e') // 2
+stringIndexOf('awesome', 'z') // -1
+*/
