0ed4e49b9c5ef77a2712ea788f943a70a2d29eed
Kateria Wynn <iamkateria@gmail.com>
2022-07-08 16:53:26 -0700

    added instructions via comments

---
+++ 012-push/index.js
@@ -0,0 +1,19 @@
+/* 
+Write a function called push which accepts two parameters, an array and any value.
+
+The function should add the value to the end of the array and then return the new length of the array.
+
+Do not use the built in Array.push() function!
+
+Examples:
+
+var arr = [1, 2, 3];
+push(arr, 10); _// 4_
+
+arr; // [1, 2, 3, 10]_
+
+var secondArr = [5, 2, 1, 6];
+push(secondArr, 20); _// 5_
+
+arr; // [5, 2, 1, 6, 20]
+*/
