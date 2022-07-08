8360237dd6d793374ef900214653180dadcbdc6a
Kateria Wynn <iamkateria@gmail.com>
2022-07-08 14:13:46 -0700

    added instructions via comment

---
+++ 008-removeFromString/index.js
@@ -0,0 +1,13 @@
+/* 
+Write a function called removeFromString, which accepts a string, a starting index (number) and a number of characters to remove.
+
+The function should return a new string with the characters removed.
+
+Examples:
+
+removeFromString('Elie', 2, 2) // 'El'
+removeFromString('Elie', 0, 1) // 'lie'
+removeFromString('Rithm School', 0, 6) // 'School'
+removeFromString('Rithm School', 2, 4) // 'RiSchool'
+removeFromString('Rithm School', 6, 400) // 'Rithm '
+*/
