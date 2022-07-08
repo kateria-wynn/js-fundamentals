f6b8cdab91bb404dbc16e6d702cb3d93b0dde2a4
Kateria Wynn <iamkateria@gmail.com>
2022-07-07 20:24:31 -0700

    added solution

--- 005-stringIndexOf/index.js
+++ 005-stringIndexOf/index.js
@@ -1,7 +1,9 @@
 /* 
-Write a function called stringIndexOf, which accepts two strings: the first is a word and the second is a single character.
+Write a function called stringIndexOf, which accepts two strings: 
+the first is a word and the second is a single character.
 
-The function should return the first index in the word at which the character exists or -1 if the character is not found.
+The function should return the first index in the word at 
+which the character exists or -1 if the character is not found.
 
 Do not use the built in String.indexOf() function!
 
@@ -10,3 +12,24 @@ Examples:
 stringIndexOf('awesome', 'e') // 2
 stringIndexOf('awesome', 'z') // -1
 */
+
+// iterate over each char in the word ✅
+// does singleChar match the char? --> grab this index and save in an array
+// --> indexes ✅
+// access the array at indexes[0] to get the first index ✅
+// doesn't have a number? return -1 ✅
+// There is a string method indexOf() but can't use. I need to have access to the
+// indexes of the strings chars so will use an old school for loop.
+
+function stringIndexOf(word, singleChar) {
+  const indexes = [];
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === singleChar) {
+      indexes.push(i);
+    }
+  }
+  console.log(Number.isInteger(indexes[0]) ? indexes[0] : -1);
+}
+stringIndexOf('awesome', 'e');
+stringIndexOf('awesome', 'z');
