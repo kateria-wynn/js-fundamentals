70a9b1602a221d19068f6da68f7fcbaade52e0b2
Kateria Wynn <iamkateria@gmail.com>
2022-07-08 17:05:06 -0700

    added solution

--- 012-push/index.js
+++ 012-push/index.js
@@ -1,19 +1,41 @@
 /* 
 Write a function called push which accepts two parameters, an array and any value.
 
-The function should add the value to the end of the array and then return the new length of the array.
+The function should add the value to the end of the array and 
+then return the new length of the array.
 
 Do not use the built in Array.push() function!
 
 Examples:
 
 var arr = [1, 2, 3];
-push(arr, 10); _// 4_
+push(arr, 10); // 4
 
-arr; // [1, 2, 3, 10]_
+arr; // [1, 2, 3, 10]
 
 var secondArr = [5, 2, 1, 6];
-push(secondArr, 20); _// 5_
+push(secondArr, 20); // 5
 
 arr; // [5, 2, 1, 6, 20]
 */
+
+// adding an element to the end
+// immediately go to the end of array and inset a new element after the last index ✅
+// --> use splice() to insert inside of the array at a specific index ✅
+// return arr.length ✅
+// There is a push() method but can't use.
+
+function push(arr, val) {
+  // insert the value at new index
+  arr.splice(arr.length + 1, 0, val);
+  return arr.length;
+}
+var arr = [1, 2, 3];
+push(arr, 10);
+
+arr;
+
+var secondArr = [5, 2, 1, 6];
+push(secondArr, 20);
+
+arr;
