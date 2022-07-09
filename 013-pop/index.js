a99f8a9266689aa43a9d35b124e9526271949422
Kateria Wynn <iamkateria@gmail.com>
2022-07-08 17:06:50 -0700

    added instructions via comments

---
+++ 013-pop/index.js
@@ -0,0 +1,17 @@
+/* 
+Write a function called pop which accepts an array.
+
+The function should remove the last value in the array and return the value removed or undefined if the array is empty.
+
+Do not use the built in Array.pop() function!
+
+Examples:
+
+var arr = [1, 2, 3, 4];
+pop(arr); _// 4_
+arr; _// [1, 2, 3]_
+
+var emptyArr = [];
+pop(emptyArr); _// undefined_
+emptyArr.length; _// 0_
+*/
