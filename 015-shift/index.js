a50077a2e65b41b2a726538b4bb7ecdc4d009d59
Kateria Wynn <iamkateria@gmail.com>
2022-07-08 17:50:33 -0700

    added solution

--- 015-shift/index.js
+++ 015-shift/index.js
@@ -1,5 +1,7 @@
 /* 
-Write a function called shift which accepts an array and removes the first value in the array and then returns the value removed. It should return undefined if the array is empty.
+Write a function called shift which accepts an array 
+and removes the first value in the array and then returns the value removed. 
+It should return undefined if the array is empty.
 
 Do not use the built in shift function!
 
@@ -8,9 +10,31 @@ Examples:
 var arr = ["a","b","c"];
 shift(arr); // "a"
 
-Copied!
 arr; // ["b","c"]
+
 var emptyArr = [];
 shift(emptyArr); // undefined
-emptyArr.length; // 0
 */
+
+// immediately accessing element at index 0 arr[0] and then return ✅
+// need to use splice() method to remove an element at a specific index ✅
+// if arr.length === 0 return undefined ✅
+// There is a built in shift function but can't use
+
+function shift(arr) {
+  let firstValue = null;
+  if (arr.length === 0) {
+    return undefined;
+  } else {
+    firstValue = arr[0];
+    arr.splice(0, 1);
+    return firstValue;
+  }
+}
+var arr = ['a', 'b', 'c'];
+shift(arr);
+
+arr;
+
+var emptyArr = [];
+shift(emptyArr);
