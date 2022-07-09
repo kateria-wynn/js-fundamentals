037482a7d1dec4042799af2cd95fd13b0522dea1
Kateria Wynn <iamkateria@gmail.com>
2022-07-08 17:35:59 -0700

    added instructions via comments

---
+++ 014-unshift/index.js
@@ -0,0 +1,16 @@
+/* 
+Write a function called unshift which accepts an array and a value 
+and adds the value to the beginning of the array.
+
+This function should return the new length of the array.
+
+Do not use the built in Array.unshift() function!
+
+Examples:
+
+var arr = [1, 2, 3];
+unshift(arr, 0); // 4
+arr; // [0, 1, 2, 3]
+
+unshift([4, 5, 6], 10); // 4
+*/
