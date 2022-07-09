2994908f96a1e579d3c872d5784b818ef307dd88
Kateria Wynn <iamkateria@gmail.com>
2022-07-09 15:22:08 -0700

    added instructions via comments

---
+++ 018-min/index.js
@@ -0,0 +1,11 @@
+/* 
+Write a function called min, which accepts an array of numbers and returns the lowest value.
+
+Do note use the built-in Math.min() function!
+
+Examples:
+
+min([5, 1, 4, 7, 1, 2]); _// 1_
+min([-1, 6, 3, 2.2, -10, -4]); _// -10_
+function min()
+*/
