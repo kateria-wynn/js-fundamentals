e5911f2ad36443c0df064be24ad5ff3312b0c2bf
Kateria Wynn <iamkateria@gmail.com>
2022-07-08 17:36:52 -0700

    added solution

--- 014-unshift/index.js
+++ 014-unshift/index.js
@@ -14,3 +14,16 @@ arr; // [0, 1, 2, 3]
 
 unshift([4, 5, 6], 10); // 4
 */
+
+// immediately putting the element in index 0 ✅
+// use splice() to insert an element in array at specific index (0) ✅
+
+function unshift(arr, val) {
+  arr.splice(0, 0, val);
+  return arr.length;
+}
+var arr = [1, 2, 3];
+unshift(arr, 0);
+arr;
+
+unshift([4, 5, 6], 10);
