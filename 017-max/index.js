534da94e3b9dbdd8e711cb502c6de4a303ca5c3f
Kateria Wynn <iamkateria@gmail.com>
2022-07-09 15:06:27 -0700

    added instructions via comments

---
+++ 017-max/index.js
@@ -0,0 +1,11 @@
+/* 
+Write a function called max, which accepts an array and returns the highest value.
+
+Do not use the built-in Math.max() function!
+
+Examples:
+
+max([5, 1, 4, 7, 1, 2]); _// 7_
+max([3, 4, 12, 1, 8]); _// 12_
+max([-1, 6, 3, 2.2, -10, -4]); _// 6_
+*/
