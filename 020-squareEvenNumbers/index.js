3f43267c2dc636a62bd3bf58351c532dc5f597f9
Kateria Wynn <iamkateria@gmail.com>
2022-07-09 16:49:07 -0700

    added instructions via comment

---
+++ 020-squareEvenNumbers/index.js
@@ -0,0 +1,9 @@
+/* 
+Write a function called squareEvenNumbers which accepts an array and returns the sum of all of the even numbers in the array squared.
+
+Examples:
+
+squareEvenNumbers([1, 2, 3, 4, 5]); _// 20_
+squareEvenNumbers([1, 3, 5, 7]); _// 0_
+squareEvenNumbers([5, 6, 7]); _// 36_
+*/
