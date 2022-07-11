a6b6bbf3221a593ac5da5d0a9af4f59070ed8e58
Kateria Wynn <iamkateria@gmail.com>
2022-07-11 15:34:59 -0700

    added solution

--- 024-entries/index.js
+++ 024-entries/index.js
@@ -1,7 +1,9 @@
 /* 
-Write a function called entries, which accepts an object and returns an array of arrays of key-value pairs.
+Write a function called entries, which accepts an object and 
+returns an array of arrays of key-value pairs.
 
-In other words, each sub-array is an "entry" in the object with two elements: the first element is the key, and the second element is the value.
+In other words, each sub-array is an "entry" in the object with two elements: 
+the first element is the key, and the second element is the value.
 
 Do not use the built in Object.entries() function!
 
@@ -18,3 +20,24 @@ _// [["first","Matt"], ["last","Lane"], ["isDogOwner",true]]_
 var obj3 = {};
 entries(obj3); _// []
 */
+
+function entries(obj) {
+  result = [];
+  if (Object.keys(obj).length === 0) {
+    return result;
+  } else {
+    for (let key in obj) {
+      result.push([key, obj[key]]);
+    }
+    return result;
+  }
+}
+
+var obj = { a: 1, b: 2, c: 3 };
+entries(obj);
+
+var obj2 = { first: 'Matt', last: 'Lane', isDogOwner: true };
+entries(obj2);
+
+var obj3 = {};
+entries(obj3);
