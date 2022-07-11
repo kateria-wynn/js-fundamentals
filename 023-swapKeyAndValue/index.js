9cf2e5953c31eec93dcbcdd6d4db69bb75f1c70b
Kateria Wynn <iamkateria@gmail.com>
2022-07-10 22:25:50 -0700

    added instructions via comments

---
+++ 023-swapKeyAndValue/index.js
@@ -0,0 +1,15 @@
+/* 
+Write a function called swapKeyAndValue, which accepts an object and a key.
+
+The function should return a new object with the given key and its value flipped, and all the other key/value pairs unchanged.
+
+Examples:
+
+var instructor = { name: 'Elie', job: 'Instructor' };
+
+swapKeyAndValue(instructor, 'name');
+_// {Elie: 'name', job: "Instructor"}_
+
+swapKeyAndValue(instructor, 'job');
+_// {name: "Elie", Instructor: 'job'}
+*/
