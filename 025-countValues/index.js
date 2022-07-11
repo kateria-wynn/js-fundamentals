f373fca02b8b565fd9ff1cadc0fd126312d3161f
Kateria Wynn <iamkateria@gmail.com>
2022-07-11 15:36:45 -0700

    added instructions via comments

---
+++ 025-countValues/index.js
@@ -0,0 +1,9 @@
+/* 
+Write a function called countValues which accepts an array and a number and returns the number of times that value appears in the array.
+
+Examples:
+
+countValues([4,1,4,2,3,4,4], 4) // 4
+countValues([4,1,4,2,3,4,4], 100) // 0
+countValues([], 1) // 0
+*/
