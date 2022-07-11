ab04d3f822ee364aa0f45838f8b2be6de45069c4
Kateria Wynn <iamkateria@gmail.com>
2022-07-10 22:16:35 -0700

    added instructions via comments

---
+++ 022-values/index.js
@@ -0,0 +1,16 @@
+/* 
+Write a function called values, which accepts an object and returns an array of all of the values in the object.
+
+Do not use the built in Object.values() function!
+
+Examples:
+
+var obj = { a: 1, b: 2, c: 3 };
+values(obj); _// [1,2,3]_
+
+var obj2 = { first: 'Matt', last: 'Lane', isDogOwner: true };
+values(obj2); _// ["Matt", "Lane", true]_
+
+var obj3 = {};
+values(obj3); _// []
+*/
