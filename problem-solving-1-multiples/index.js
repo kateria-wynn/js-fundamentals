8cd1f7b952f5dac693e0a0b9312e54dd03ea4d99
Kateria Wynn <iamkateria@gmail.com>
2022-07-11 20:12:27 -0700

    added solution

--- problem-solving-1-multiples/index.js
+++ problem-solving-1-multiples/index.js
@@ -9,3 +9,19 @@ Examples:
 multiples(3, 4) // [3, 6, 9, 12]
 multiples(2, 5) // [2, 4, 6, 8, 10]
 */
+
+// x is num
+// n multiples
+function multiples(x, n) {
+  const result = [];
+  let currNum = x;
+
+  while (result.length < n) {
+    result.push(currNum);
+    currNum = currNum + x;
+  }
+  return result;
+}
+
+multiples(3, 4);
+multiples(2, 5);
