40bd0b2149a162d9a4724cb9ecbd4a0aaa5857dd
Kateria Wynn <iamkateria@gmail.com>
2022-07-11 23:15:40 -0700

    addedd instructions via comments

---
+++ problem-solving-1-pluck/index.js
@@ -0,0 +1,19 @@
+/* 
+Write a function called pluck, which takes an array of objects and the name of a key.
+
+The function should return an array containing the value associated with that key for each object, or undefined if that key is not present in the object.
+
+Examples:
+
+pluck([
+  { name: "Tim" }, { name: "Matt" }, { name: "Elie" }],
+ 'name'
+)
+// ["Tim", "Matt", "Elie"]
+
+pluck(
+  [{ name: "Tim", isBoatOwner: true }, { name: "Matt", isBoatOwner: false }, { name: "Elie" }],
+ 'isBoatOwner'
+)
+// [true, false, undefined]
+*/
