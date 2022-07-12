04ff59d6980f7571ddf030805ccfb316822ea49d
Kateria Wynn <iamkateria@gmail.com>
2022-07-11 20:02:58 -0700

    added instructions via comments

---
+++ problem-solving-1-multiples/index.js
@@ -0,0 +1,11 @@
+/* Implement a function called multiples that accepts two numbers: x and n.
+
+The function should return the first n multiples of the number x.
+
+Assume that x is a positive integer.
+
+Examples:
+
+multiples(3, 4) // [3, 6, 9, 12]
+multiples(2, 5) // [2, 4, 6, 8, 10]
+*/
