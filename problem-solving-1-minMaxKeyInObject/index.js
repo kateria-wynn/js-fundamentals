5222fbe556cf1b273c649de91a7db83dbfd73ba2
Kateria Wynn <iamkateria@gmail.com>
2022-07-13 17:15:44 -0700

    added instructions via comment

---
+++ problem-solving-1-minMaxKeyInObject/index.js
@@ -0,0 +1,13 @@
+/* 
+Write a function called minMaxKeyInObject that accepts an object with numeric keys.*
+
+The function should return an array with the following format: [lowestKey, highestKey]
+
+Examples:
+
+minMaxKeyInObject({ 2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e' });
+// [1, 10]
+minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' });
+// [1, 4]
+Remember all object keys are strings, even if they hold numeric values.
+*/
