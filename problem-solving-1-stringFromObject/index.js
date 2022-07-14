1d61d5e61c11abed2848ba6f48d9c398e07b1901
Kateria Wynn <iamkateria@gmail.com>
2022-07-13 18:43:45 -0700

    added instructions via comments

---
+++ problem-solving-1-stringFromObject/index.js
@@ -0,0 +1,16 @@
+/* 
+Write a function called stringFromObject that generates a string from an object's key/value pairs.
+
+The format should be "key = value, key = value".
+
+Each key/value pair should be separated by a comma and space except for the last pair.
+
+Examples:
+
+stringFromObject({ a: 1, b: '2' });
+_// "a = 1, b = 2"_
+stringFromObject({ name: 'Elie', job: 'Instructor', isCatOwner: false });
+_// "name = Elie, job = Instructor, isCatOwner = false"_
+stringFromObject({});
+_// ""_
+*/
