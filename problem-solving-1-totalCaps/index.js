3ebb61d0bdd1423283b3c20ced2f6448bc3229ea
Kateria Wynn <iamkateria@gmail.com>
2022-07-13 20:28:37 -0700

    added solution

--- problem-solving-1-totalCaps/index.js
+++ problem-solving-1-totalCaps/index.js
@@ -7,3 +7,26 @@ totalCaps(["AwesomE", "ThIngs", "hAppEning", "HerE"]) // 8
 totalCaps(["Elie", "Matt", "Tim"]) // 3
 totalCaps(["hello", "world"]) // 0
 */
+// create count var --> set 0
+// loop over each element in arr for..of loop
+// each el is a string --> loop over each string
+// check each char in string --> char === char.toUpperCase()
+// if it does --> add one to count
+// if not, count = count;
+
+function totalCaps(arr) {
+  let count = 0;
+  for (let str of arr) {
+    for (let char of str) {
+      if (char === char.toUpperCase()) {
+        count++;
+      } else {
+        count = count;
+      }
+    }
+  }
+  return count;
+}
+totalCaps(['AwesomE', 'ThIngs', 'hAppEning', 'HerE']);
+totalCaps(['Elie', 'Matt', 'Tim']);
+totalCaps(['hello', 'world']);
