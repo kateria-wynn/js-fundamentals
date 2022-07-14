18fe2e48077779ef971820bf3f18508563703c27
Kateria Wynn <iamkateria@gmail.com>
2022-07-13 18:00:01 -0700

    added solution

--- problem-solving-1-minMaxKeyInObject/index.js
+++ problem-solving-1-minMaxKeyInObject/index.js
@@ -11,3 +11,28 @@ minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' });
 // [1, 4]
 Remember all object keys are strings, even if they hold numeric values.
 */
+// loop through the obj ✅
+// pull out each key --> change the key to a number --> Number(); ✅
+// push each key into an array ✅
+// Math.min() --> set these as variables ✅
+// Math.max() ✅
+// result.push(min, max); ✅
+
+function minMaxKeyInObject(obj) {
+  const objKeys = [];
+  const result = [];
+  let min = null;
+  let max = null;
+
+  for (let key in obj) {
+    objKeys.push(Number(key));
+  }
+  min = Math.min(...objKeys);
+  max = Math.max(...objKeys);
+
+  result.push(min, max);
+  return result;
+}
+
+minMaxKeyInObject({ 2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e' });
+minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' });
